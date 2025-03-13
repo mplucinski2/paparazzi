f5301dcd49e79d88eeb4055db3aea295ae028229
Miantao Zhao <m.zhao-8@student.tudelft.nl>
2025-03-13 23:47:56 +0100

    commiting experimental new config where i can mess around

---
+++ sw/airborne/modules/experiment1/experiment1.h
@@ -0,0 +1,14 @@
+/**
+ * @file "modules/experiment1/experiment1.h"
+ * @author MianTao Zhao
+ * expirementing around in paparazzi stuff
+ */
+
+
+#ifndef ORANGE_AVOIDER_H
+#define ORANGE_AVOIDER_H
+
+extern void experiment1_init(void);
+extern void experiment1_periodic(void);
+
+#endif
