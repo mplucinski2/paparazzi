2b8839338af74eb2c0aaa01ed810d287cdbdd988
dionemmpap <emmanouil03@gmail.com>
2025-03-26 14:14:04 +0100

    Trying to fix ROI (unsuccessful)

--- sw/airborne/modules/computer_vision/cv_detect_color_object.h
+++ sw/airborne/modules/computer_vision/cv_detect_color_object.h
@@ -25,32 +25,39 @@
  * if you are over the defined color_object or not
  */
 
-#ifndef COLOR_OBJECT_DETECTOR_CV_H
-#define COLOR_OBJECT_DETECTOR_CV_H
-
-#include <stdint.h>
-#include <stdbool.h>
-
-// Module settings
-extern uint8_t cod_lum_min1;
-extern uint8_t cod_lum_max1;
-extern uint8_t cod_cb_min1;
-extern uint8_t cod_cb_max1;
-extern uint8_t cod_cr_min1;
-extern uint8_t cod_cr_max1;
-
-extern uint8_t cod_lum_min2;
-extern uint8_t cod_lum_max2;
-extern uint8_t cod_cb_min2;
-extern uint8_t cod_cb_max2;
-extern uint8_t cod_cr_min2;
-extern uint8_t cod_cr_max2;
-
-extern bool cod_draw1;
-extern bool cod_draw2;
-
-// Module functions
-extern void color_object_detector_init(void);
-extern void color_object_detector_periodic(void);
-
-#endif /* COLOR_OBJECT_DETECTOR_CV_H */
+ #ifndef COLOR_OBJECT_DETECTOR_CV_H
+ #define COLOR_OBJECT_DETECTOR_CV_H
+ 
+ #include <stdint.h>
+ #include <stdbool.h>
+ 
+ // Module settings
+ extern uint8_t cod_lum_min1;
+ extern uint8_t cod_lum_max1;
+ extern uint8_t cod_cb_min1;
+ extern uint8_t cod_cb_max1;
+ extern uint8_t cod_cr_min1;
+ extern uint8_t cod_cr_max1;
+ 
+ extern uint8_t cod_lum_min2;
+ extern uint8_t cod_lum_max2;
+ extern uint8_t cod_cb_min2;
+ extern uint8_t cod_cb_max2;
+ extern uint8_t cod_cr_min2;
+ extern uint8_t cod_cr_max2;
+ 
+ extern bool cod_draw1;
+ extern bool cod_draw2;
+ 
+ // ROI settings for camera 1
+ extern uint16_t roi_x_min1;
+ extern uint16_t roi_x_max1;
+ extern uint16_t roi_y_min1;
+ extern uint16_t roi_y_max1;
+ extern bool use_roi1;
+ 
+ // Module functions
+ extern void color_object_detector_init(void);
+ extern void color_object_detector_periodic(void);
+ 
+ #endif /* COLOR_OBJECT_DETECTOR_CV_H */
